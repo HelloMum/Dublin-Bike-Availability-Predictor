ac52df88a35d12e77b0c6bcccfff408ae79368bb
HelloMum <jdg@hotmail.es>
2024-02-15 12:47:32 +0000

    Configure the cron routine for bike scrapper

---
+++ Scrapper/Bikes/bike_cron_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Execute this program in the AWS instance by using "sudo bash bike_cron_setup.sh" with execution permissions
+
+sleep 5
+
+echo "This program will check if the cron entry for bike_api_test.py every 5 minutes in home folder, if it is not there it will create one"
+echo "Create CRON entry"
+
+# The entry will be added with the normal python install. This should be running from miniconda once is setup
+new_entry="5 * * * * python /home/bike_api_test.py"
+
+# This will check to see if is already there
+if ! crontab -l | fgrep -q "$new_entry"; then
+    #write out current crontab
+    crontab -l > allcrons
+    #echo new cron into cron file
+    echo "$new_entry" >> allcrons
+    display_all_crons allcrons
+    #install new cron file
+    crontab allcrons
+    rm allcrons
+fi
