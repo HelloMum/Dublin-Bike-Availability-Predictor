a89006d67390626e8fd6b21f1f09d856b1158509
HelloMum <jdg@hotmail.es>
2024-02-19 20:02:16 +0000

    Checks for dynamic changes and updates the DB

--- Scrapper/Bikes/bike_scrapper_dynamic.py
+++ Scrapper/Bikes/bike_scrapper_dynamic.py
@@ -1,5 +1,7 @@
 import mysql.connector
 import requests
+import logging
+from datetime import datetime, timezone
 
 # Connection to the database
 cnx = mysql.connector.connect(
@@ -13,63 +15,56 @@ cnx = mysql.connector.connect(
 API_KEY = '8e2d39566206c680b37fa532dc58214a023b6783'
 CONTRACT_NAME = 'dublin'
 
-def create_dynamic_table(cursor):
-    try:
-        cursor.execute("\n"
-                       "            CREATE TABLE IF NOT EXISTS stations_dynamic (\n"
-                       "                id INT AUTO_INCREMENT PRIMARY KEY,\n"
-                       "                number INT,\n"
-                       "                contract_name VARCHAR(255),\n"
-                       "                name VARCHAR(255),\n"
-                       "                address VARCHAR(255),\n"
-                       "                latitude DECIMAL(10, 6),\n"
-                       "                longitude DECIMAL(10, 6),\n"
-                       "                banking BOOLEAN,\n"
-                       "                bonus BOOLEAN,\n"
-                       "                status VARCHAR(255),\n"
-                       "                last_update TIMESTAMP,\n"
-                       "                available_bikes INT,\n"
-                       "                available_bike_stands INT,\n"
-                       "                bike_stands INT\n"
-                       "            )\n"
-                       "        ")
-        cnx.commit()
-        print("Table 'stations_dynamic' created successfully.")
-    except mysql.connector.Error as err:
-        print(err.msg)
+# Set up logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+# Log to console
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+# Also log to a file, useful for the cron routine
+file_handler = logging.FileHandler("cpy-errors.log")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 def stations_to_db_dynamic(cursor, station):
+    utc_now = datetime.now(timezone.utc)
+    formatted_utc_now = utc_now.strftime('%Y-%m-%d %H:%M:%S')  # Remove the last 3 digits to get milliseconds
+    last_update_timestamp = station.get('last_update') / 1000.0
+    last_update_datetime = datetime.utcfromtimestamp(last_update_timestamp)
+
+
     vals = (
         station.get('number'),
-        station.get('contract_name'),
         station.get('name'),
-        station.get('address'),
-        station.get('position').get('latitude'),
-        station.get('position').get('longitude'),
         station.get('banking'),
         station.get('bonus'),
         station.get('status'),
-        station.get('last_update'),
+        last_update_datetime,  # Use the converted datetime from Unix
+        formatted_utc_now,  # The last time we updated the API
         station.get('available_bikes'),
         station.get('available_bike_stands'),
         station.get('bike_stands'),
     )
 
     try:
-        cursor.execute("\n"
-                       "            INSERT INTO stations_dynamic (\n"
-                       "                number, contract_name, name, address, latitude, longitude, banking, bonus,\n"
-                       "                status, last_update, available_bikes, available_bike_stands, bike_stands\n"
-                       "            )\n"
-                       "            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)\n"
-                       "        ", vals)
+        cursor.execute(
+            "INSERT INTO stations_dynamic ("
+            "    number, name, banking, bonus, status, last_update, api_update,"
+            "    available_bikes, available_bike_stands, bike_stands"
+            ")"
+            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+            vals
+        )
         cnx.commit()
         print("Dynamic data inserted successfully")
     except mysql.connector.Error as err:
         print(err.msg)
         print("Error: Maybe duplicated")
-
 
 
 STATIONS_URL = f"https://api.jcdecaux.com/vls/v1/stations?apiKey={API_KEY}&contract={CONTRACT_NAME}"
@@ -78,10 +73,7 @@ response = requests.get(STATIONS_URL)
 
 if response.status_code == 200:
     stations_json = response.json()
-
     cursor = cnx.cursor()
-    create_dynamic_table(cursor)  # Create table if not exists
-
     for station in stations_json:
         stations_to_db_dynamic(cursor, station)
 
