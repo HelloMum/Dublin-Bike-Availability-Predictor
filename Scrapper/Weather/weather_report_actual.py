e86feda931b9587b589761f1e68016c0a3a648f5
HelloMum <jdg@hotmail.es>
2024-02-21 10:28:13 +0000

    Now program exit gracefully. And fallbacks.

--- Scrapper/Weather/weather_report_actual.py
+++ Scrapper/Weather/weather_report_actual.py
@@ -2,8 +2,9 @@ import mysql.connector
 import requests
 import logging
 from datetime import datetime, timezone
+import sys
 
-API_key = '2f9e606fb12c8f3e219615414ab757ff'
+API_key = 'acdd67e7c31cde91a7b39ce7b279d504'
 # Coordinates of Dublin 53.3498° N, 6.2603° W
 lat = '53.3498'
 lon = '6.2603'
@@ -32,15 +33,8 @@ logger.addHandler(handler)
 file_handler = logging.FileHandler("weather-errors.log")
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
-
 response = requests.get(weather_api_url)
 
-if response.status_code == 200:
-    weather_data = response.json()
-    cursor = cnx.cursor()
-    store_weather_data(cursor, weather_data)
-else:
-    print(f"Failed to fetch weather data. Status code: {response.status_code}")
 
 def store_weather_data(cursor, data):
     current_time = datetime.utcfromtimestamp(data['current']['dt']).strftime('%Y-%m-%d %H:%M:%S')
@@ -48,7 +42,7 @@ def store_weather_data(cursor, data):
     # Extract relevant data
     temp = data['current']['temp']
     main_event = data['current']['weather'][0]['main']
-    rain_hour_day = data['daily'][1]['rain'] #just daily hourly
+    rain_hour_day = data['daily'][1]['rain']  # just daily hourly
     feels_like = data['current']['feels_like']
     humidity = data['current']['humidity']
     wind_speed = data['current']['wind_speed']
@@ -61,13 +55,30 @@ def store_weather_data(cursor, data):
                        "                main_event, rain_hour_day, timestamp, temperature, feels_like, humidity, wind_speed, description\n"
                        "            )\n"
                        "            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)\n"
-                       "        ", (main_event, rain_hour_day, current_time,  temp , feels_like, humidity, wind_speed, description))
+                       "        ",
+                       (main_event, rain_hour_day, current_time, temp, feels_like, humidity, wind_speed, description))
         cnx.commit()
         print("Weather data inserted successfully")
         cursor.close()
+        cnx.close()
+        sys.exit()
     except mysql.connector.Error as err:
         print(err.msg)
         print("Error: Maybe duplicated?")
+        cursor.close()
+        cnx.close()
+        sys.exit()
 
 
-cnx.close()
+if response.status_code == 200:
+    weather_data = response.json()
+    cursor = cnx.cursor()
+    store_weather_data(cursor, weather_data)
+else:
+    print(f"Failed to fetch weather data. Status code: {response.status_code}")
+    print("Exiting...")
+    cnx.close()
+    sys.exit()
+
+print("Exiting...")
+sys.exit()
