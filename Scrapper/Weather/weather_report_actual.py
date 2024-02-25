d1cb5dab1e1ff925456a67bafebd04807f063222
HelloMum <jdg@hotmail.es>
2024-02-25 15:01:25 +0000

    The API will omit a field when the value is 0 or null, included those cases in the weather script

--- Scrapper/Weather/weather_report_actual.py
+++ Scrapper/Weather/weather_report_actual.py
@@ -43,13 +43,55 @@ def store_weather_data(cursor, data):
     current_time = datetime.utcfromtimestamp(data['current']['dt']).strftime('%Y-%m-%d %H:%M:%S')
 
     # Extract relevant data
-    temp = data['current']['temp']
-    main_event = data['current']['weather'][0]['main']
-    rain_hour_day = data['daily'][1]['rain']  # just daily hourly
-    feels_like = data['current']['feels_like']
-    humidity = data['current']['humidity']
-    wind_speed = data['current']['wind_speed']
-    description = data['current']['weather'][0]['description']
+    # ~  We carefully take the data as some element might not be present on the JSON
+    try:
+        temp = data['current']['temp']
+    except KeyError:  # If the current temp is not present it will send 999 to indicate wrong info
+        print("No temp skipping")
+        logging.debug("No temp skipping")
+        temp = 0
+
+    try:
+        main_event = data['current']['weather'][0]['main']
+    except KeyError:  # If in the hour there is 0 rain it won't show on the JSON
+        print("No main event, skipping")
+        logging.debug("No main event, skipping")
+        main_event = "Not defined"
+
+    try:
+        rain_hour_day = data['hourly'][0]['rain']  # Hourly rain expected
+    except KeyError:  # If in the hour there is 0 rain it won't show on the JSON
+        print("No hourly rain, skipping")
+        logging.debug("No hourly rain, skipping")
+        rain_hour_day = 0
+
+    try:  # I have not seen these not been present but I will add them just in case
+        feels_like = data['current']['feels_like']
+    except KeyError:  # If in the hour there is 0 rain it won't show on the JSON
+        print("No hourly rain, skipping")
+        logging.debug("No hourly rain, skipping")
+        feels_like = 0
+
+    try:
+        humidity = data['current']['humidity']
+    except KeyError:  # If in the hour there is 0 rain it won't show on the JSON
+        print("No humidity, skipping")
+        logging.debug("No humidity, skipping")
+        humidity = 0
+
+    try:
+        wind_speed = data['current']['wind_speed']
+    except KeyError:  # If in the hour there is 0 rain it won't show on the JSON
+        print("No wind_speed, skipping")
+        logging.debug("No wind_speed, skipping")
+        wind_speed = 0
+
+    try:
+        description = data['current']['weather'][0]['description']
+    except KeyError:  # If in the hour there is 0 rain it won't show on the JSON
+        print("No description, skipping")
+        logging.debug("No description, skipping")
+        rain_hour_day = "No description"
 
     # Store data in the database
     try:
@@ -64,12 +106,14 @@ def store_weather_data(cursor, data):
         print("Weather data inserted successfully")
         cnx.close()
         sys.exit()
+
     except mysql.connector.Error as err:
         print(err.msg)
         print("Error: Maybe duplicated?")
         cnx.close()
         sys.exit()
-    except:
+
+    finally:
         cnx.close()
         sys.exit()
 
@@ -85,4 +129,5 @@ else:
     sys.exit()
 
 print("Exiting...")
+
 sys.exit()
