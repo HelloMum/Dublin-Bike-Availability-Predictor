c58d760841076b02d996e81da1f767f50f4946d2
HelloMum <jdg@hotmail.es>
2024-02-26 18:40:05 +0000

    Changed the name of the folder, restructured the code to work with new one.

---
+++ Flask/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, jsonify
+from DBinterface import Link
+import config
+
+# Initialize Google Maps client and app instance
+app = Flask(__name__)
+app.config["MAPS_APIKEY"] = config.GOOGLE_MAPS_API_KEY
+
+# MySQL configurations
+DBconfig = config.CNX
+session = Link(DBconfig)
+
+print("Starting app...")
+
+@app.route("/")
+def index():
+    static_stations = session.get_static_all_stations()
+    return render_template('index.html', stations=static_stations, MAPS_APIKEY=app.config["MAPS_APIKEY"])
+
+@app.route("/stations_dynamic")
+def get_static_stations():
+    static_stations = session.get_static_all_stations()
+    return render_template('index.html', stations=static_stations, MAPS_APIKEY=app.config["MAPS_APIKEY"])
+
+@app.route("/weather")
+def get_weather_now():
+    weather_data = session.get_weather()
+    return jsonify(weather_data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, use_debugger=False, use_reloader=False)
