c9446f0837123495edf65340f5e920e76c1754f0
HelloMum <jdg@hotmail.es>
2024-02-26 17:22:30 +0000

    Now the DInterface does the tasks for the database interface.

--- Bikes_Flask/app.py
+++ Bikes_Flask/app.py
@@ -1,111 +1,37 @@
 from flask import Flask, render_template, jsonify
-from flaskext.mysql import MySQL
+from DBinterface import Link
 import config
 
+# Initialize Google Maps client and app instance
 app = Flask(__name__)
-
-# Initialize Google Maps client
-app.config["MAPS_APIKEY"] = config.GOOGLE_MAPS_API_KEY  
+app.config["MAPS_APIKEY"] = config.GOOGLE_MAPS_API_KEY
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = config.DATABASE_CONFIG['user']
-app.config['MYSQL_DATABASE_PASSWORD'] = config.DATABASE_CONFIG['password']
-app.config['MYSQL_DATABASE_DB'] = config.DATABASE_CONFIG['database']
-app.config['MYSQL_DATABASE_HOST'] = config.DATABASE_CONFIG['host']
+DBconfig = config.CNX
+session = Link(DBconfig)
 
-mysql = MySQL()
-mysql.init_app(app)
+print("Starting app...")
+
 
 @app.route("/")
 def index():
-    """
-    Renders the index.html template with a list of stations and the Google Maps API Key.
+    session.populate_map_all()
+    static_stations = session.get_static_stations()
+    print(static_stations)
+    return render_template('index.html', stations=static_stations, MAPS_APIKEY=app.config["MAPS_APIKEY"])
 
-    Returns:
-        The rendered template.
-    """
-    stations = []
-    try:    
-        cursor = mysql.connect().cursor()
-        cursor.execute("SELECT * FROM stations_static")
-        rows = cursor.fetchall() 
-        for row in rows:
-            stations.append({
-                'number': row[0],
-                'name': row[1],
-                'latitude': row[2],
-                'longitude': row[3],
-                'title': row[1],
-                'status': row[8],
-                'bike_stands': row[7],
-                'available_bikes': row[6]
-            })
-        cursor.close()
-    except Exception as e:
-        print(f"Error fetching data from database: {str(e)}")
-        
-    return render_template('index.html', stations = stations, MAPS_APIKEY=app.config["MAPS_APIKEY"])
 
 @app.route("/stations_dynamic")
-def dynamic():
-    """
-    Renders the index.html template with a list of dynamic stations and the Google Maps API Key.
-
-    Returns:
-        The rendered template.
-    """
-    stations = []
-    try:    
-        cursor = mysql.connect().cursor()
-        cursor.execute("SELECT * FROM stations_dynamic")
-        rows = cursor.fetchall() 
-        for row in rows:
-            stations.append({
-                'number': row[0],
-                'name': row[1],
-                'latitude': row[2],
-                'longitude': row[3],
-                'title': row[1],
-                'status': row[8],
-                'bike_stands': row[7],
-                'available_bikes': row[6]
-            })
-        cursor.close()
-    except Exception as e:
-        print(f"Error fetching data from database: {str(e)}")
-        
-    return render_template('index.html', stations = stations, MAPS_APIKEY=app.config["MAPS_APIKEY"])
+def get_static_stations():
+    static_stations = session.get_static_stations()
+    return render_template('index.html', stations=static_stations, MAPS_APIKEY=app.config["MAPS_APIKEY"])
 
 
 @app.route("/weather")
-def weather():
-    """
-    Retrieves the latest weather data from the database.
-
-    Returns:
-        The weather data in JSON format.
-    """
-    weather_data = {}
-    try:    
-        cursor = mysql.connect().cursor()
-        cursor.execute("SELECT * FROM weather_data ORDER BY timestamp DESC LIMIT 1")
-        row = cursor.fetchone()
-        if row:
-            weather_data = {
-                'main_event': row[0],
-                'rain_hour_day': row[1],
-                'timestamp': row[2],
-                'temperature': row[3],
-                'feels_like': row[4],
-                'humidity': row[5],
-                'wind_speed': row[6],
-                'description': row[7]
-            }
-        cursor.close()
-    except Exception as e:
-        print(f"Error fetching data from database: {str(e)}")
-        
+def get_weather_now():
+    weather_data = session.get_weather()
     return jsonify(weather_data)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, use_debugger=False, use_reloader=False)
