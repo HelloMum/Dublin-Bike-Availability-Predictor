ba609c93232e4801db5de07c6da87fb87f7bd0f1
HelloMum <jdg@hotmail.es>
2024-02-29 10:04:38 +0000

    Missing some json conversion, but the connection connects successfully to the app at the start

--- Flask/DBinterface.py
+++ Flask/DBinterface.py
@@ -2,10 +2,8 @@ from flask import jsonify
 
 
 class Link:
-    database = False
     def __init__(self, database):
         print("Created connection to database")
-        self.session = "Normal"
         self.database = database
 
     def __enter__(self):
@@ -21,45 +19,39 @@ class Link:
         # Close cursor and database connection
         print("Bye")
         self.cursor.close()
+        return
 
-    def _get_dynamic_all_stations(self):
+    def get_dynamic_all_stations_last(self):
         with self:  # Leverage the context manager for resource management
             self.cursor.execute(
                 'SELECT * FROM stations_dynamic ORDER BY last_update LIMIT 114')
             rows = self.cursor.fetchall()
-            self.dynamic_all_stations = rows  # Store raw data, not jsonify(rows)
+            dynamic_all_stations = rows  # Store raw data, not jsonify(rows)
 
             # Print data for debugging
             print("dyna Stations (raw):", rows)
-            return
+            return dynamic_all_stations
 
-    def _get_static_all_stations(self):
+    def get_static_all_stations_all(self):
         with self:
             self.cursor.execute(
                 'SELECT * FROM stations_static ORDER BY place_name')
             rows = self.cursor.fetchall()
-            self.static_all_stations = rows  # Store raw data, not jsonify(rows)
+            static_all_stations = rows  # Store raw data, not jsonify(rows)
 
             # Print data for debugging
             print("Static Stations (raw):", rows)
-            return
+            return static_all_stations
 
-    def get_weather(self):
+    def get_weather_last(self):
         with self:
             self.cursor.execute("SELECT * FROM weather_data ORDER BY timestamp DESC LIMIT 1")
             weather_data = self.cursor.fetchone()
             # Print data for debugging
-            print("Weather Data (raw):", row)
+            print(weather_data)
 
-            return jsonify(weather_data)  # Return raw data
+        return weather_data  # Return raw data
 
-    def get_dynamic_all_stations(self):
-        self._get_dynamic_all_stations()
-        return self.dynamic_all_stations  # Return raw data
-
-    def get_static_all_stations(self):
-        self._get_static_all_stations()
-        return self.static_all_stations  # Return raw data
 
 
 if __name__ == "__main__":
