deb204a457655cbe9c9acf171d20ad05b76aab08
HelloMum <jdg@hotmail.es>
2024-03-29 12:06:39 +0000

    Convert sql into a more convenient format for Data analysis

---
+++ DataAnalytics/sql_converter.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import re
+
+# Read the SQL dump file
+dump_file_path = 'demo.sql'
+with open(dump_file_path, 'r') as file:
+    sql_dump_content = file.read()
+
+# ext
+def extract_table_data(table_name, sql_dump_content):
+    data_rows = re.findall(fr'INSERT INTO `{table_name}` VALUES \((.*?)\);', sql_dump_content)
+    data = [row.strip('\'').split(',') for row in data_rows]
+    return data
+
+# Define a function to export data to CSV and create a dataframe too
+def export_to_csv(table_name, data):
+    if not os.path.exists("exported_data"):
+        os.makedirs("exported_data")
+    data_frame = pd.DataFrame(data, columns=['id', 'number', 'name', 'banking', 'bonus', 'status',
+                                             'last_update', 'api_update', 'available_bikes',
+                                             'available_bike_stands', 'bike_stands'])
+    data_frame = data_frame.applymap(lambda x: x.strip('`'))
+    csv_file_name = f'exported_data/{table_name}.csv'
+    data_frame.to_csv(csv_file_name, index=False)
+    print(f"Data from table '{table_name}' exported to '{csv_file_name}' check for more information.")
+
+# Setting up different tables
+tables_to_export = ['stations_dynamic']
+for table_name in tables_to_export:
+    table_data = extract_table_data(table_name, sql_dump_content)
+    export_to_csv(table_name, table_data)
